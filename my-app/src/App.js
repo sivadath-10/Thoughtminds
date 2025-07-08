34a4bfe74c0bab47e534de3b1bf614fc0459789c
Your Name <your@email.com>
2025-07-09 00:23:20 +0530

    Initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,31 @@
-
+import React, { useState } from 'react';
 import './App.css';
-
-function App() {
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+const App = () => {
   return (
-    <div className="App">
-      <h1>hey</h1>
+    <div className='App'>
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder='username' required />
+          <FaUser className='icon' />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="password" required />
+         <FaLock className='icon'/>
+        </div>
+        <div className="remember-forgot">
+          <label><input type="checkbox" /></label>
+          <a href="#">forgot password</a>
+        </div>
+        <button type="submit">login</button>
+        <div className="register-link">
+          <p>don't have an account? <a href="#">register</a></p>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
