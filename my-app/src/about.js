bd0b3cb5994c2332ad28e0050f771996b0b37a58
Your Name <your@email.com>
2025-07-14 21:40:36 +0530

    added 10 user name and password and added validation only 10 users can login the form

--- my-app/src/about.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function About() {
-  return <h2>This is the About Page</h2>;
-}
-
-export default About;
--- my-app/src/components/LoginPage.jsx
+++ my-app/src/components/LoginPage.jsx
@@ -1,21 +1,37 @@
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../App.css';
 import { FaUser, FaLock } from "react-icons/fa";
 
+const users = [
+  {  username: "Bret"},
+  {  username: "Antonette" },
+  {  username: "Samantha" },
+  {  username: "Karianne" },
+  {  username: "Kamren" },
+  {  username: "Leopoldo_Corkery" },
+  {  username: "Elwyn.Skiles" },
+  {  username: "Maxime_Nienow" },
+  {  username: "Delphine" },
+  {  username: "Moriah.Stanton" }
+];
+
 const LoginPage = () => {
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
 
-
-    if (username.trim() !== '') {
   
+    const validUser = users.find(user => user.username === username);
+
+    if (validUser && password === `${username}@123`) {
       navigate('/profile', { state: { username } });
     } else {
-      alert("Enter a valid username.");
+      alert("Invalid username or password");
     }
   };
 
@@ -25,29 +41,23 @@ const LoginPage = () => {
         <h1>Login</h1>
 
         <div className="input-box">
-          <input
-            type="text"
-            placeholder="Username"
-            required
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <input type="text" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
           <FaUser className='icon' />
         </div>
 
         <div className="input-box">
-          <input type="password" placeholder="Password" required />
+          <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
           <FaLock className='icon' />
         </div>
 
         <div className="remember-forgot">
-          <a href="#">forgot password</a>
+          <a href="#">Forgot password?</a>
         </div>
 
         <button type="submit">Login</button>
 
         <div className="register-link">
-          <p>Don't have an account? <a href="#">register</a></p>
+          <p>Don't have an account? <a href="#">Register</a></p>
         </div>
       </form>
     </div>
